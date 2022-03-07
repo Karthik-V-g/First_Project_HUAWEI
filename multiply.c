5150ce20185423766dc5fb8adf8030f0164acfad
karthik <karthikkarthik14112000@gmail.com>
2022-03-07 18:38:18 +0530

    multiply.c file is commited second

---
+++ multiply.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() 
+{    
+    int number1=1, number2=2, multiply;
+    multiply = number1 * number2;       
+    printf("%d * %d = %d", number1, number2, multiply);
+    return 0;
+}
