82f86000f3f5cb427617478e0b2c0b11a87878c2
Karthik-V-g <68321621+Karthik-V-g@users.noreply.github.com>
2022-03-07 19:30:10 +0530

    added comment

--- add.c
+++ add.c
@@ -3,6 +3,7 @@ int main()
 {    
     int number1=1, number2=2, sum;
     sum = number1 + number2;       
+    //Addition of two integers
     printf("%d + %d = %d", number1, number2, sum);
     return 0;
 }
