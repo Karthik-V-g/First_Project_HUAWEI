96f912bb51cfb98006290c0c43fbdd7647056caf
karthik <karthikkarthik14112000@gmail.com>
2022-03-07 18:36:40 +0530

    subtract.c file is commited second

---
+++ subtract.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() 
+{    
+    int number1=2, number2=1, sub;
+    sum = number1 - number2;       
+    printf("%d +- %d = %d", number1, number2, sub);
+    return 0;
+}
